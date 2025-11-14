bb49b124a982ec4766d93bea61d0bf81a3532018
javier garcia alvarez <javigarcial87@gmail.com>
2025-11-14 21:26:50 +0100

    Refactor HTML structure and update JavaScript fetch logic for Pokémon information retrieval

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const button = document.getElementById('get-pokemon');
 
+/*
 button.addEventListener('click', () => {
     fetch("https://pokeapi.co/api/v2/pokemon/ditto")
         .then((response) => response.json())
@@ -11,3 +11,50 @@ button.addEventListener('click', () => {
             console.log("Error obteniendo los personajes:", error);
         });
     });
+
+/*
+    .then ((data) => {
+    
+        const personajes = data.results.map(personaje => {
+            const template = `
+                <li class="perso">
+                    <img src="${personaje.image}" alt="${personaje.name}" >
+                    <h3><b>Name: </b>${personaje.name}</h3>
+                    <p><b>Especies: </b>${personaje.species}</p>
+                </li>
+            `
+
+*/
+const button = document.getElementById('get-pokemon');
+const pokemonName = document.getElementById('pokemon-select').value
+const info = document.getElementById('pokemon-info');
+
+button.addEventListener('click', () => {
+    const pokemonName = select.value;
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then(response => response.json())
+    .then((data) => {
+      info.innerHTML = `
+        <li class="pokInfo">
+            <h3>${data.name}</h3>
+            <p>${personaje.species}</p>
+        </li>
+
+      `
+    })
+    .catch(error => {
+      console.log("Error obteniendo los personajes:", error);
+    });
+});
+//nombre=name, imagen=sprites.front_default, tipo=data.types[0], altura=height y peso= weight
+
+/*
+<li class="perso">
+    <img src="${personaje.image}" alt="${personaje.name}" >
+    <h3><b>Name: </b>${personaje.name}</h3>
+                                <p><b>Especies: </b>${personaje.species}</p>
+                </li>
+
+                // <img src="${prites.front_default} " alt="imagPok">
+
+    */
