9a5ffc126c1abd8e566ec05a11fe9df6325dfa62
javier garcia alvarez <javigarcial87@gmail.com>
2025-11-14 21:30:56 +0100

    Remove unused code and clean up event listener for Pokémon retrieval

--- js/script.js
+++ js/script.js
@@ -1,32 +1,5 @@
-
-/*
-button.addEventListener('click', () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-        .then((response) => response.json())
-        .then(data => {
-             displayCharacters(data.results);
-            console.log("DATA RECIBIDA:", data);
-        })
-        .catch(error => {
-            console.log("Error obteniendo los personajes:", error);
-        });
-    });
-
-/*
-    .then ((data) => {
-    
-        const personajes = data.results.map(personaje => {
-            const template = `
-                <li class="perso">
-                    <img src="${personaje.image}" alt="${personaje.name}" >
-                    <h3><b>Name: </b>${personaje.name}</h3>
-                    <p><b>Especies: </b>${personaje.species}</p>
-                </li>
-            `
-
-*/
 const button = document.getElementById('get-pokemon');
-const pokemonName = document.getElementById('pokemon-select').value
+const select = document.getElementById('pokemon-select').value
 const info = document.getElementById('pokemon-info');
 
 button.addEventListener('click', () => {
